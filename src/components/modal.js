ee0e301a64a7bba46ffdfb60c3f8f2778a9ec29d
Ansar <ansarmaksutov10@gmail.com>
2026-03-16 14:57:57 +0500

    добавил валидацию форм и запросы

--- src/components/modal.js
+++ src/components/modal.js
@@ -20,6 +20,12 @@ export function openModal(popup) {
 }
 
 export function closeModal(popup) {
+    const cardAddInputName = document.querySelector('.popup__input_type_card-name');
+    const cardAddInputUrl = document.querySelector('.popup__input_type_url');
+    cardAddInputName.value = '';
+    cardAddInputUrl.value = '';
+    const popUpAvatarInput = document.querySelector('#avatar-input');
+    popUpAvatarInput.value = '';
     popup.classList.remove('popup_is-opened');
     document.body.removeEventListener('keydown', allPopupClose);
     document.body.removeEventListener('click', allPopupClose);
--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
+import { getUser, getCards, addCard, deleteCard, addLike, removeLike, editProfile, editAvatar } from './components/api.js';
 import { closeModal, openModal} from "./components/modal.js";
-import {createCard} from "./components/card.js";
-import {likeFunc} from "./components/card.js";
-import {deleteFunc} from "./components/card.js";
-import { initialCards } from "./cards.js";
 import './index.css';
 
 const template = document.querySelector('#card-template').content;
-const card = template.cloneNode(true);
 const formNewPlace = document.forms['new-place'];
 const formEditProfile = document.forms['edit-profile'];
 const cardsContainer = document.querySelector('.places__list');
 const popUpEdit = document.querySelector('.popup_type_edit');
 const profileEditButton = document.querySelector('.profile__edit-button');
+const popUpProfileImg = document.querySelector('.popup_type_new-avatar');
 const popUpInputName = document.querySelector('.popup__input_type_name');
 const popUpInputDescription = document.querySelector('.popup__input_type_description');
 const profileTitle = document.querySelector('.profile__title');
+const profileImage = document.querySelector('.profile__image');
 const profileDescription = document.querySelector('.profile__description');
 const cardAddInputName = document.querySelector('.popup__input_type_card-name');
 const cardAddInputUrl = document.querySelector('.popup__input_type_url');
@@ -23,68 +21,332 @@ const popUpImage = popUpTypeImg.querySelector('.popup__image');
 const cardAddButton = document.querySelector('.profile__add-button');
 const popUpAddCard = document.querySelector('.popup_type_new-card');
 const imgPopupCaption = document.querySelector('.popup__caption');
-export function allPopupClose(evt) {
-    const openedPopup = document.querySelector('.popup_is-opened');
-    if (evt.key === 'Escape') {
-        closeModal(openedPopup);
-    }
-    if (evt.target.closest('.popup__close')) {
-        closeModal(openedPopup);
-    }
-    if (evt.target === openedPopup) {
-        closeModal(openedPopup);
-    }
-}
+const formNewAvatar = document.forms['new-avatar'];
+const popUpAvatarInput = document.querySelector('#avatar-input');
+
+formEditProfile.addEventListener('input', (evt) => {
+    checkEditValidity(evt.target);
+});
+formNewPlace.addEventListener('input', (evt) => {
+    checkCardValidity(evt.target);
+});
+formNewAvatar.addEventListener('input', (evt) => {
+    checkAvatarValidity(evt.target);
+});
 profileEditButton.addEventListener('click', () => {
     popUpInputName.value = profileTitle.textContent;
     popUpInputDescription.value = profileDescription.textContent;
     openModal(popUpEdit);
 });
-cardAddButton.addEventListener('click', () => {
+cardAddButton.addEventListener('click', (evt) => {
+    const submitButton = formNewPlace.querySelector('button');
+    submitButton.disabled = true;
+    submitButton.classList.add('button-disabled');
     openModal(popUpAddCard);
 });
+profileImage.addEventListener('click', (evt) => {
+    const submitButton = formNewAvatar.querySelector('button');
+    submitButton.disabled = true;
+    submitButton.classList.add('button-disabled');
+    openModal(popUpProfileImg);
+})
+formNewPlace.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const submitButton = formNewPlace.querySelector('button');
+    const submitDefaultText = formNewPlace.querySelector('.button').textContent;
+    const submitLoaderText = 'Сохранение';
+    submitButton.textContent = submitLoaderText;
+    const name = cardAddInputName.value;
+    const link = cardAddInputUrl.value;
+
+    addCard(name, link)
+        .then(response => response.json())
+    .then(cardsData => {
+        const templateElement = template.cloneNode(true);
+        const img = templateElement.querySelector('.card__image');
+        const title = templateElement.querySelector('.card__title');
+
+        img.src = link;
+        title.textContent = name;
+
+        cardsContainer.prepend(templateElement);
+        submitButton.textContent = 'Сохранение';
+        closeModal(popUpAddCard);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+        .finally(() => {
+            submitButton.textContent = submitDefaultText;
+        })
+});
+
+formEditProfile.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    const newName = popUpInputName.value;
+    const newAbout = popUpInputDescription.value;
+    const submitButton = formEditProfile.querySelector('button');
+    const submitDefaultText = formEditProfile.querySelector('.button').textContent;
+    const submitLoaderText = 'Сохранение';
+    submitButton.textContent = submitLoaderText;
+    editProfile(newName, newAbout)
+        .then((response) => {
+            return response.json()
+        })
+    .then((data) => {
+        profileTitle.textContent = newName;
+        profileDescription.textContent = newAbout;
+        submitButton.textContent = 'Сохранение';
+        closeModal(popUpEdit);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+        .finally(() => {
+            submitButton.textContent = submitDefaultText;
+        })
+});
+
+formNewAvatar.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const submitButton = formNewAvatar.querySelector('button');
+    const submitDefaultText = formNewAvatar.querySelector('.button').textContent;
+    const submitLoaderText = 'Сохранение';
+    submitButton.textContent = submitLoaderText;
+    const newAvatar = popUpAvatarInput.value;
+    editAvatar(newAvatar)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            profileImage.style.backgroundImage = `url(${newAvatar})`;
+            closeModal(popUpProfileImg);
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+        .finally(() => {
+            submitButton.textContent = submitDefaultText;
+        })
+})
+
+// Edit form validation
+
+
+// Передадим текст ошибки вторым параметром
+const showInputError = (element, errorMessage) => {
+    const formError = formEditProfile.querySelector(`.${element.id}-error`)
+    element.classList.add('form__input_type_error');
+// Заменим содержимое span с ошибкой на переданный параметр
+    formError.textContent = errorMessage;
+    formError.classList.add('form__input-error_active');
+};
+const hideInputError = (element) => {
+    const formError = formEditProfile.querySelector(`.${element.id}-error`)
+    element.classList.remove('form__input_type_error');
+    formError.classList.remove('form__input-error_active');
+// Очистим ошибку
+    formError.textContent = '';
+};
+const checkEditValidity = (input) => {
+    const regex = /^[a-zA-Zа-яА-ЯёЁ\s-]+$/;
+    const popUpInputName = document.querySelector('#name-input');
+    const popUpInputDescription = document.querySelector('#description-input');
+    const submitButton = formEditProfile.querySelector('button');
+    if (!regex.test(input.value)) {
+        input.setCustomValidity('Разрешены только латинские и кириллические буквы, дефис и пробел');
+    } else {
+        input.setCustomValidity('');
+    }
+    if (!input.validity.valid){
+        showInputError(input, input.validationMessage)
+    } else {
+        hideInputError(input);
+    }
+    if (popUpInputName.validity.valid && popUpInputDescription.validity.valid) {
+        submitButton.disabled = false;
+        submitButton.classList.remove('button-disabled');
+    } else {
+        submitButton.disabled = true;
+        submitButton.classList.add('button-disabled');
+    }
+};
+
+// addNewcard form validation
+
+const showError = (element, errorMessage) => {
+    const formError = formNewPlace.querySelector(`.${element.id}-error`);
+    element.classList.add('form__input_type_error');
+    formError.textContent = errorMessage;
+    formError.classList.add('form__input-error_active');
+};
+
+const hideError = (element) => {
+    const formError = formNewPlace.querySelector(`.${element.id}-error`);
+    element.classList.remove('form__input_type_error');
+    formError.classList.remove('form__input-error_active');
+    formError.textContent = '';
+};
+
+const checkCardValidity = (input) => {
+    const regex = /^[a-zA-Zа-яА-ЯёЁ\s-]+$/;
+
+    if (input !== cardAddInputUrl) {
+        if (!regex.test(input.value)) {
+            input.setCustomValidity('Разрешены только латинские и кириллические буквы, дефис и пробел');
+        } else {
+            input.setCustomValidity('');
+        }
+    } else {
+        input.setCustomValidity('');
+    }
+
+    if (!input.validity.valid){
+        showError(input, input.validationMessage);
+    } else {
+        hideError(input);
+    }
+
+    const button = formNewPlace.querySelector('.button');
+    if (cardAddInputName.validity.valid && cardAddInputUrl.validity.valid) {
+        button.disabled = false;
+        button.classList.remove('button-disabled');
+    } else {
+        button.disabled = true;
+        button.classList.add('button-disabled');
+    }
+}
+
+
+// update profile image
+
+const showErrorMessage = (element, errorMessage) => {
+    const formError = formNewAvatar.querySelector(`.${element.id}-error`);
+    element.classList.add('form__input_type_error');
+    formError.textContent = errorMessage;
+    formError.classList.add('form__input-error_active');
+};
+
+const hideErrorMessage = (element) => {
+    const formError = formNewAvatar.querySelector(`.${element.id}-error`);
+    element.classList.remove('form__input_type_error');
+    formError.classList.remove('form__input-error_active');
+    formError.textContent = '';
+};
+const checkAvatarValidity = (input) => {
+
+    if (!input.validity.valid){
+        showErrorMessage(input, input.validationMessage);
+    } else {
+        hideErrorMessage(input);
+    }
+
+    const button = formNewAvatar.querySelector('.button');
+    if (input.validity.valid) {
+        button.disabled = false;
+        button.classList.remove('button-disabled');
+    } else {
+        button.disabled = true;
+        button.classList.add('button-disabled');
+    }
+}
+
+
+// const getUser = fetch (`https://practicetasks.kz/tagspot/users/me`, {
+//         headers: {
+//             authorization: '826752daf4464a1c899218c2fde56babkDrgkLDTGB'
+//         }
+//     })
+//     .then((response) => {
+//         return response.json()
+//     });
+// const getCards = fetch (`https://practicetasks.kz/tagspot/cards`, {
+//         headers: {
+//             authorization: '826752daf4464a1c899218c2fde56babkDrgkLDTGB',
+//             'Content-Type': 'application/json'
+//         }
+// })
+//     .then((response) => {
+//         return response.json()
+//     });
+
+Promise.all([getUser(), getCards()])
+    .then(([userData, cardsData]) => {
+        profileTitle.textContent = userData.name;
+        profileDescription.textContent = userData.about;
+        profileImage.style.backgroundImage = `url(${userData.avatar})`;
+
+        cardsData.forEach(card => {
+            const templateElement = template.cloneNode(true);
+            const img = templateElement.querySelector('.card__image');
+            const title = templateElement.querySelector('.card__title');
+            const likeCounter = templateElement.querySelector('.card__likes');
+            const deleteButton = templateElement.querySelector('.card__delete-button');
+            const likeButton = templateElement.querySelector('.card__like-button');
+            deleteButton.id = card.id;
+            likeButton.id = card.id;
+            if (card.owner.id !== userData.id) {
+                deleteButton.remove();
+            }
+            img.src = card.link;
+            title.textContent = card.name;
+            likeCounter.textContent = card.likes.length;
+            cardsContainer.append(templateElement);
+        })
+    })
+    .catch(err => console.error(err));
 
 cardsContainer.addEventListener('click', (evt) => {
+    const card = evt.target.closest('.card');
+    const likeCounter = card.querySelector('.card__likes');
+    if (evt.target.classList.contains('card__delete-button')) {
+        fetch(`https://practicetasks.kz/tagspot/cards/${evt.target.id}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: '826752daf4464a1c899218c2fde56babkDrgkLDTGB',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((response) => {
+                if (response.ok) {
+                    evt.target.closest('.card').remove();
+                }
+            })
+        .catch(err => console.log(err));
+    }
+    if (evt.target.classList.contains('card__like-button_is-active')) {
+        fetch(`https://practicetasks.kz/tagspot/cards/likes/${evt.target.id}`,{
+            method: 'DELETE',
+            headers: {
+                authorization: '826752daf4464a1c899218c2fde56babkDrgkLDTGB',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                evt.target.classList.remove('card__like-button_is-active');
+                likeCounter.textContent = data.likes.length;
+            })
+        .catch(err => console.log(err));
+    } else if (!evt.target.classList.contains('card__like-button_is-active') && evt.target.classList.contains('card__like-button')) {
+        fetch(`https://practicetasks.kz/tagspot/cards/likes/${evt.target.id}`,{
+            method: 'PUT',
+            headers: {
+                authorization: '826752daf4464a1c899218c2fde56babkDrgkLDTGB',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                evt.target.classList.add('card__like-button_is-active');
+                likeCounter.textContent = data.likes.length;
+            })
+        .catch(err => console.log(err));
+    }
     if (evt.target.classList.contains('card__image')) {
         popUpImage.src = evt.target.closest('.card__image').src;
         imgPopupCaption.textContent = evt.target.closest('.card').querySelector('.card__title').textContent;
         openModal(popUpTypeImg);
     }
 });
-
-initialCards.forEach(item => {
-    cardsContainer.append(createCard(item, template));
-});
-
-formNewPlace.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-
-    const card = template.cloneNode(true);
-    const img = card.querySelector('.card__image');
-    const title = card.querySelector('.card__title');
-
-    img.src = cardAddInputUrl.value;
-    title.textContent = cardAddInputName.value;
-    cardAddInputUrl.value = '';
-    cardAddInputName.value = '';
-
-    cardsContainer.prepend(card);
-    closeModal(popUpAddCard);
-})
-formEditProfile.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-
-    profileTitle.textContent = popUpInputName.value;
-    profileDescription.textContent = popUpInputDescription.value;
-
-    closeModal(popUpEdit);
-});
-cardsContainer.addEventListener('click', (evt) => {
-    likeFunc(evt);
-    deleteFunc(evt);
-})
-
-
-
-
-
